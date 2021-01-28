e5c851506db1a9f765a26025a13425741cdd8fd0
Michael Conrad <m.conrad.202@gmail.com>
2021-01-28 12:37:31 -0500

    xxxy :q

--- misc/TestScene.gd
+++ misc/TestScene.gd
@@ -1,13 +1,27 @@
 extends Node
 
+var marker_ps:PackedScene = preload("res://misc/TestMarker.tscn")
+
 onready var map = $LevelMap
 onready var player = $Player
+onready var markers = $Markers
+onready var camera = $Camera2D
 
 func _ready():
+	
+	player.cameraFollow.remote_path = camera.get_path()
+	
+	map.level=1
+	map.generate()
 	var p:Vector2
 	p = map.randomPortal()
 	p = map.randomPortal()
-	p.x += 128+96
-	p.y += 128+96
+	p = map.randomPortal()
+	p = map.randomPortal()
 	player.setGlobalPosition(p)
 	print(str(p))
+
+	for portal in map.portals:
+		var marker = marker_ps.instance()
+		markers.add_child(marker)
+		marker.global_position=portal

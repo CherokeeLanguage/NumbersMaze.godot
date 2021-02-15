02625d014ed2d65c703df8eb320fb8ef84d66687
Michael Conrad <m.conrad.202@gmail.com>
2021-02-15 18:25:36 -0500

    fix bug caused by type check

--- nodes/FloorPortal.gd
+++ nodes/FloorPortal.gd
@@ -5,6 +5,7 @@ class_name FloorPortal
 signal player_in_portal
 
 func _on_Area2D_body_entered(body: Node) -> void:
-	if Utils.is_type(body, "PlayerNode"):
+	print("floor portal body = "+body.name)
+	if body is PlayerNode:
 		print("player_in_portal")
 		emit_signal("player_in_portal")

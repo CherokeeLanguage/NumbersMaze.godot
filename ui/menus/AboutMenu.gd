ec2b689abdcc892fa5559c717a74b1dfb3a94371
Michael Conrad <m.conrad.202@gmail.com>
2021-02-16 13:53:20 -0500

    Functioning about scroller with actual info

--- ui/menus/AboutMenu.gd
+++ ui/menus/AboutMenu.gd
@@ -3,21 +3,27 @@ extends BaseMenu
 class_name AboutMenu
 
 onready var scroller:ScrollContainer = $VBoxContainer/ScrollContainer
+onready var text_box:Label = $VBoxContainer/ScrollContainer/VBoxContainer/Label
 
 var position:float = 0
 
 func _ready() -> void:
 	container = $VBoxContainer
+	var text: = AboutTextClass.new()
+	text_box.text=text.text
 
 func _physics_process(delta):
 	if Input.is_action_pressed("up"):
-		position-=500*delta
+		position-=1000*delta
 
 	if Input.is_action_pressed("down"):
-		position+=500*delta
+		position+=1000*delta
 
 	scroller.scroll_vertical=position
 	position = clamp(position, 0, scroller.scroll_vertical+1)
 	
-		
 	._physics_process(delta)
+
+func load_text()->void:
+	
+	return
--- update-changelog.sh
+++ update-changelog.sh
@@ -5,6 +5,16 @@ cd "$(dirname "$0")"
 
 if [ ! -d text ]; then mkdir text; fi
 
-git log --pretty=reference > text/changelog.txt
-
-
+(
+echo "--- CHANGELOG ---" 
+echo
+git log --pretty=reference
+echo
+echo "--- Previous Repo: NumbersMaze ---"
+echo
+cat text/changelog.prev2.txt
+echo
+echo "--- Previous Repo: CherokeeNumbersMaze ---"
+echo
+cat text/changelog.prev1.txt
+) > text/changelog.txt
